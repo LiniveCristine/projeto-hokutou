e0f0d49a47a3bb53335515d37513ce5ab4bd4d4c
Linive Cristine <linive.dev@gmailmail.com>
2026-05-13 09:50:21 -0300

    Adicionando função cadastrar cliente

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-from config.db import  criar_conexao
+from services.user_services import inset_client 
 
 
-conn = criar_conexao()
+
+
+while True:
+    print('''
+
+        \t*** BEM VINDO AO HOKUTOU **\n
+          1- Cadastre-se\n
+          2- login\n
+          3- Sair\n
+
+    ''')
+
+    try:
+
+        op =int(input("* Digite a opção: "))
+
+        if op == 1:
+            nome = input("\nDigite seu user: ")
+            senha= input("Digite sua senha: ")
+            data_nsc = input("Digite sua data de nascimento (AAAA-DD-MM): ")
+
+            inset_client(nome, senha, data_nsc)
+
+
+        elif op == 2:
+            pass
+    
+        else:
+            break
+
+    except ValueError:
+        print("\n*** Opção invalida!! ***\n")
--- services/user_services.py
+++ services/user_services.py
@@ -1,2 +1,20 @@
 from config.db import criar_conexao
 
+
+def inset_client(nome:str, senha:str, data_nsc: str):
+    try:
+        con= criar_conexao()
+        cursor = con.cursor()
+
+        sql = "INSERT INTO cliente(nome,senha, data_nsc) VALUES(%s,%s,%s)"
+        cursor.execute(sql, (nome, senha, data_nsc))
+
+        con.commit()
+        print(f"\nCliente {nome} cadastrado\n")
+
+    except Exception as e:
+        print(f"\nErro ao inserir cliente: {e}")
+
+    finally:
+        cursor.close()
+        con.close()
