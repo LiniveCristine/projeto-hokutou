ace8d7e1c05646ab4dd448fa0c30a36736048fcc
Linive Cristine <linive.dev@gmailmail.com>
2026-05-20 18:30:57 -0300

    Ajustando menu inicial

--- layouts/menu_inicial.py
+++ layouts/menu_inicial.py
@@ -1,25 +1,24 @@
 from services.products_services import listar_produtos
 
 def menu_inicial():
-    print('''
-
-        \t*** MENU INICIAL ***\n
-          1- Listar Produtos\n
-          2- Carrinho\n
-          3- Endereço Cadastrado\n
-          4- Sair\n
-
-    ''')
     while True:         #While adicionado
 
         try:
+            print('''
+
+            \t*** MENU INICIAL ***\n
+            1- Listar Produtos\n
+            2- Carrinho\n
+            3- Endereço Cadastrado\n
+            4- Sair\n
+
+            ''')
 
             op =int(input("* Digite a opção: "))
 
             if op == 1:
                 listar_produtos()
                 
-
 
             elif op == 2:
                 print("\n\t*** CARRINHO ***\n")
@@ -28,6 +27,7 @@ def menu_inicial():
             elif op == 3:
                 print("\n\t*** ENDEREÇO CADASTRADO ***\n")
                 pass
+            
             elif op == 4:                   #opção 4 pra sair colocada, meio que serve como logout(?)
                 print("SAINDO DO SISTEMA!")
                 break
